33888f0623b8d1008664cbceabf3a5bd584c6883
Özgür Güner <43880017+ozguner00@users.noreply.github.com>
2024-11-17 14:26:03 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -17,7 +17,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (needle && *needle == '\0')
+	if (*needle == '\0')
 		return ((char *)haystack);
 	i = 0;
 	while (haystack[i] != '\0' && i < len && needle != 0)
